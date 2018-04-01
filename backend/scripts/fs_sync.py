351fbe6d970e7bffa17c79797056a6f9f3268a63
Ben0p <31563479+Ben0p@users.noreply.github.com>
2018-04-02 04:28:41 +0800

    Messing with time and space
    Fixing up baclend time module

---
+++ backend/scripts/fs_sync.py
@@ -0,0 +1,79 @@
+import com
+import serial
+import time
+import nmea
+
+
+'''
+Farmstar time lord module
+Processes GPS, NTP and local times 
+optional 6 digit time value HHMMSS
+optional comport to check for GPS data
+optional nmea sentence
+optional ntp server ip
+Returns a bunch of time values and status in dictionary format
+'''
+
+class lord():
+
+    def __init__(self, comport=None, line='', NTPserver='',fix=000000):
+        self.comport = comport
+        self.line = line
+        self.NTPserver = NTPserver
+        self.NTPstatus = False
+        self.status = 'Initializing'
+        self.fix = fix
+        self.ser = None
+
+        
+
+
+
+    
+    def gpsTime(self):
+        
+        while True:
+            try:
+                if(self.ser == None or self.line == ''):
+                    self.ser = serial.Serial(self.comport,9600,timeout=1.5)
+                self.line = ser.readline().decode("utf-8") # Read the entire string
+                self.gps_time = nmea.parse(line).GGA['Fix']
+            except:
+                if(not(ser == None)):
+                    ser.close()
+                    ser = None
+                    self.comstatus = "Disconnecting"
+                self.comstatus = "No Connection to {}".format(self.comport)
+                time.sleep(2)
+
+    def getPorts(self):
+        print("Scanning for active ports...")
+        self.comports = com.Ports().valid
+        if self.comports == []:
+            print("No active ports")
+            self.comport = None
+        else:
+            self.comport = self.comports[0]
+            
+    
+    def gpsTime(self, line):
+        print("Parsing GPS time...")
+        self.gps_time = nmea.parse(line).GGA['Fix']
+
+
+    def getNTP():
+        #Attempts to retrieve system NTP server
+        #Tests NTP server or tries default
+        #Returns NTP time
+        print("Polling NTP server...")
+        pass
+
+
+    def setTime(time):
+        #Sets the system time
+        os.system('time %s' % (time))
+
+
+
+if __name__ == '__main__':
+    lord()
